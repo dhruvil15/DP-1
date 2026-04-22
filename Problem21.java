b40436681ebb4c427fe9fb123e93428506323ca6
dhruvil15 <76967484+dhruvil15@users.noreply.github.com>
2026-04-21 20:03:35 -0400

    Complete20_21

---
+++ Problem21.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if (n == 1) return nums[0];
+
+        int prev = nums[0];
+        int curr = Math.max(nums[0], nums[1]);
+
+        for (int i = 2; i < n; i++) {
+            int temp = curr;
+            curr = Math.max(temp, prev+nums[i]);
+            prev = temp;
+        }
+
+        return curr;
+    }
+}
